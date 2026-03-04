13f0cc690d4e8ed149953c262955f2dfd736e298
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-03-05 02:19:08 +0530

    day1 Airtribe

--- JavaScript_Refresher/day1_airtribe.js
+++ JavaScript_Refresher/day1_airtribe.js
@@ -66,3 +66,92 @@ console.log("myName after assigning null:", myName); // null
 //  both are doing the same thing, but null is more intentional and clear to indicate that the variable is empty or has no value,
 //  while undefined can be a result of a variable being declared but not assigned a value or a function that does not return anything.
 
+// Non-primitive data types - Object, Array, Function
+
+// ------------------ Object -------------------------
+
+const hello = "Hello World";
+const age1 = 28;
+const person = {
+    name: "Mohankumar",
+    age: 28,
+    isCool: true
+};
+
+console.log(person);
+console.log("Name:", person.name);
+console.log("Age:", person.age);
+console.log("Is Cool:", person.isCool);
+
+//------------------- Array -------------------------
+
+const numbers = [1, 2, 3, 4, 5];
+const fruits = ["Apple", "Banana", "Orange"];
+const mixedArray = [1, "Hello", true, null, undefined, { name: "Mohankumar" }];
+
+console.log(numbers);
+console.log(fruits);
+console.log(mixedArray);
+
+//------------------- Function -------------------------
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+greet("Mohankumar"); // Hello, Mohankumar!
+console.log(greet("Mohankumar")); // Hello, Mohankumar!
+
+// Arrow function
+const greetArrow = (name) => {
+    return `Hello, ${name}!`;
+}
+
+console.log(greetArrow("Mohankumar")); // Hello, Mohankumar!
+
+// -------------------- Typeof Operator -------------------------
+
+console.log(typeof firstName); // string
+console.log(typeof age);
+console.log(typeof isHeCool);
+console.log(typeof emptyValue);
+console.log(typeof unassignedVariable);
+console.log(typeof person);
+console.log(typeof numbers);
+console.log(typeof greet);
+
+// -----------PARSING AND EXECUTION OF JAVASCRIPT-----------------
+
+// 1. Parsing - JavaScript engine reads the code and converts it into an Abstract Syntax Tree (AST) and checks for syntax errors.
+// 2. Execution - JavaScript engine executes the code line by line and performs the operations as defined in the code.
+
+// Example 1
+
+var x = 10; // Declaration and assignment
+console.log(x); // 10
+
+x = 20; // Reassignment
+console.log(x); // 20
+
+// Example 2 (this concept is know as HOISTING)
+
+console.log(a)
+var a = 20;
+console.log(a)
+
+// Example 3 - functions
+
+console.log("-------------------");
+var z;
+console.log(z);
+
+function sum(num1, num2) {
+    const sum_agg = num1 + num2;
+    return sum_agg;
+}
+
+const result = sum(2, 3);
+console.log("result",result); 
+ 
+z = 12
+console.log(z);
