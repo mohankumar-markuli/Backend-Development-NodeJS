7003d71eec5b23fe56d37442376cd842782dd8e7
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-03-07 00:29:08 +0530

    first node program

---
+++ NodeJS/firstProgram/add-numbers.js
@@ -0,0 +1,9 @@
+function add_numbers(a, b) {
+    const result = a + b;
+    console.log(`The sum of ${a} and ${b} is: ${result}`);
+}
+
+// add_numbers(5, 10);
+// console.log(module)
+
+module.exports = add_numbers;
