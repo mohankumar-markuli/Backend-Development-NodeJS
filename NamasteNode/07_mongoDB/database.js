17d54e58acc8076935d65c178cf4163129e80bde
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-03-27 21:37:12 +0530

    mongoDB

---
+++ NamasteNode/07_mongoDB/database.js
@@ -0,0 +1,43 @@
+// step 1: Go to mongoBD
+// step 2: create a free M0 cluster
+// step 3: create a user
+// step 4: get the connection string
+// step 5: to access data inside use MBD Compass software
+// step 6: MongoDB NodeJS Driver - npm install mongodb 
+
+const { MongoClient } = require("mongodb");
+
+const url =
+    "mongodb+srv://mohankumarmarkuli_db_user:H3mYMhJOQHqOsv6F@namastenode.voiccvs.mongodb.net/?appName=NamasteNode";
+const client = new MongoClient(url);
+
+const dbName = 'HelloWorld';
+
+async function main() {
+
+    await client.connect();
+    console.log("Connection successful to server");
+    const db = client.db(dbName);
+    const collection = db.collection('user');
+
+    // writing/inserting data to collection
+    const data = {
+        "firstname": "Chandrayi",
+        "lastname": "Gowda",
+        "city": "Hassan"
+    }
+
+    const insertResult = await collection.insertMany([data]);
+    console.log("Inserted document =>", insertResult);
+
+    // Reading data from collection
+    const findResult = await collection.find({}).toArray();
+    console.log("Found Document =>", findResult);
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
