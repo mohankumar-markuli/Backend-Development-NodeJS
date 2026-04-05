79bf9fad1d6a3f9147a9199bbefbb9cc90b5b1ef
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-04-06 01:23:04 +0530

    Mangoose init  - POST /register and /login

---
+++ NodeJS/05_AuthenticationAndAuthorization/01_MongodbInit/src/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { logger } = require('./middlewares/loggerMiddleware');
+const courseRouter = require('./routes/courseRoute');
+
+const app = express(); //instance of express
+
+// Why drivers insted of REST
+// can we use REST API for DBs > => yes
+// MongoDB in the past used to use REST with Auth layers
+// CouchDB, PouchDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mohankumarmarkuli_db_user:xqNc3f4CpDbDFfQc@airtribenodejs.nlllbtl.mongodb.net/?appName=AirtribeNodeJS";
+
+app.use(logger);
+app.use('/api/v1/courses', courseRouter); // mount router on the path /api/v1/courses
+
+app.get('/', (req, res) => {
+    res.send("Domain Page : Hello world!....")
+}
+);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// start the server
+app.listen(3000, () => {
+    console.log("Express server up and running");
+})
---
+++ NodeJS/05_AuthenticationAndAuthorization/02_MongooseInit/src/routes/userRoute.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser } = require('../controllers/userController');
+
+router.use(express.json());
+
+router.post("/register", async (req, res) => {
+    const user = req.body;
+    const dbUser = await registerUser(user);
+    res.send(dbUser);
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await loginUser(email, password);
+        return res.status(200).send({ token: token });
+    } catch (err) {
+        res.send("invalid credientials")
+    }
+});
+
+
+module.exports = router;
