f17dcb044e47d811d9991335ad1bd58b744ace39
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-04-11 01:46:52 +0530

    Oauth

---
+++ NodeJS/05_AuthenticationAndAuthorization/04_OAuth/src/controllers/courseControllers.js
@@ -0,0 +1,21 @@
+// const courses = require('../models/coursesModel').courses;
+const courseModel = require("../models/courseModel");
+
+const createCourse = async (course) => {
+    const dbCourse = await courseModel.create(course);
+    return dbCourse;
+}
+
+const getAllCourse = async () => {
+    const dbCourses = await courseModel.find({});
+    if (!dbCourses) throw new Error("No data found");
+    return dbCourses;
+}
+
+const getCourseById = async (id) => {
+    const course = await courseModel.findById(id);
+    if (!course) throw new Error("Course with given id not found");
+    return course;
+}
+
+module.exports = { getAllCourse, getCourseById, createCourse };
---
+++ NodeJS/05_AuthenticationAndAuthorization/04_OAuth/src/routes/userRoute.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser } = require('../controllers/userController');
+
+router.use(express.json());
+
+router.post("/register", async (req, res) => {
+    const user = req.body;
+
+    try {
+        const dbUser = await registerUser(user);
+        res.send(dbUser);
+    } catch (err) {
+        console.log(err.message)
+        res.status(400).send("User Already Exsist - Try Login");
+    }
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await loginUser(email, password);
+        return res.status(200).send({ token: token });
+    } catch (err) {
+        console.log(err);
+        res.send("invalid credientials")
+    }
+});
+
+module.exports = router;
